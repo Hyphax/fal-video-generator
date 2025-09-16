d744ed4b70c981b4d245c7e823be713c1a4d9647
Zayn Ali <121715300+Hyphax@users.noreply.github.com>
2025-09-16 09:54:20 +0000

    Update GET method to fetch with no-cache

--- app/api/result/[id]/route.ts
+++ app/api/result/[id]/route.ts
@@ -1,10 +1,11 @@
-import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
 
 export async function GET(_: Request, { params }: { params: { id: string } }) {
-  const WAN_API = process.env.WAN_API_URL!;
-  const r = await fetch(`${WAN_API}/result/${params.id}`);
-  const buf = Buffer.from(await r.arrayBuffer());
-  return new NextResponse(buf, {
+  const r = await fetch(`${process.env.WAN_API_URL}/result/${params.id}`, {
+    cache: "no-store",
+  });
+  const buf = await r.arrayBuffer();
+  return new Response(buf, {
     headers: {
       "Content-Type": "video/mp4",
       "Content-Disposition": `inline; filename="${params.id}.mp4"`,
